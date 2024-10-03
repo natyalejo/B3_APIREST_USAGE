fcd8437ed2d044beee5d841ae73266f659aec253
Alejandro norden <alejonorden@gmail.com>
2024-10-03 14:07:52 -0500

    mi primer Api

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { ApiCoffeeCarruselBasic} from './components/ApiCoffeCarruselBasic'
+
+function App() {
+
+
+  return (
+    <div>
+      <ApiCoffeeCarruselBasic />
+    </div>
+  )
+}
+
+export default App
